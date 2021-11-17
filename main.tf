7b2275de66ec860fff31ac5155b086c7f8406a0d
Andrew Amuzu <andrew.amuzu@gmail.com>
2021-11-17 17:10:15 -0500

    correcting org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "Tafa-Terrform-GHdemo"
+    organization = "Tafa-Terraform-GHdemo"
 
     workspaces {
       name = "gh-actions-demo"
